9b99dbe0f88f332881c9fc68af3a5f99c0bbf229
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-02-15 23:08:34 +0900

    Create page4

---
+++ lib/page4.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Page4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Page 4'),
+        )
+      ],
+    );
+  }
+}
