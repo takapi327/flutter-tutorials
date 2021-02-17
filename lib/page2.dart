465b6ac29045b713a14c7c87167a82252fd78361
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-02-17 23:21:24 +0900

    Add App state

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Page2 extends StatelessWidget {
   @override
@@ -7,9 +10,71 @@ class Page2 extends StatelessWidget {
       children: [
         Container(
           padding: EdgeInsets.all(10.0),
-          child: Text('Page 2'),
+          child: MyCatalog()
         )
       ],
     );
+  }
+}
+
+class MyCatalog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        myCartSection(),
+        addCartButton()
+      ],
+    );
+  }
+
+  Widget myCartSection() {
+    return Consumer<CartModel>(
+      builder: (context, cart, child) => Stack(
+        children: [
+          Text("Total price: ${cart.totalPrice}"),
+        ],
+      ),
+    );
+  }
+
+  Widget addCartButton() {
+    return Consumer<CartModel>(
+      builder: (context, cart, child) {
+        return Container(
+          child: FlatButton(
+            child: Text('Add'),
+            onPressed: () {
+              cart.add(new Item());
+            }
+          )
+        );
+      }
+    );
+
+  }
+}
+
+class Item extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  }
+}
+
+class CartModel extends ChangeNotifier {
+  final List<Item> _items = [];
+
+  UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
+
+  int get totalPrice => _items.length * 42;
+
+  void add(Item item) {
+    _items.add(item);
+    notifyListeners();
+  }
+
+  void removeAll() {
+    _items.clear();
+    notifyListeners();
   }
 }
