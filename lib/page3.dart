0c8dd6116708c71e9b765f68ba79a7c72d472bf4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-03-30 23:55:38 +0900

    Error commit

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:stripe_payment/stripe_payment.dart';
+import 'package:stripe_sdk/stripe_sdk_ui.dart';
 
 class Page3 extends StatelessWidget {
+  GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey();
+  final formKey = new GlobalKey<FormState>();
+  final card    = new StripeCard();
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          padding: EdgeInsets.all(10.0),
-          child: Text('Page 3'),
+        CardForm(card: card, formKey: formKey),
+        FlatButton(
+          child: Text("決済する"),
+          onPressed: () async {
+            if (formKey.currentState.validate()) {
+              formKey.currentState.save();
+
+              final CreditCard _creditCard = CreditCard(
+                number:   card.number,
+                expMonth: card.expMonth,
+                expYear:  card.expYear
+              );
+              StripePayment.createTokenWithCard(_creditCard).then((token) async {
+                final url = 'http://localhost:9000/api/stripe/payment';
+                await http.post(
+                  url,
+                  body: {
+                    'name':          'TEST TOMINAGA',
+                    'email':         'test@ezweb.ne.jp',
+                    'phone':         '09012345678',
+                    'paymentMethod': token
+                  }
+                );
+              });
+            } else {
+              print('処理が通りませんでした。');
+            }
+          }
         )
       ],
     );
