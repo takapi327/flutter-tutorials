fd92e2e42187e89964933e8dbac615ee9e49d8ef
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-02 20:34:50 +0900

    Fix Error

--- lib/page3.dart
+++ lib/page3.dart
@@ -9,38 +9,40 @@ class Page3 extends StatelessWidget {
   final card    = new StripeCard();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CardForm(card: card, formKey: formKey),
-        FlatButton(
-          child: Text("決済する"),
-          onPressed: () async {
-            if (formKey.currentState.validate()) {
-              formKey.currentState.save();
-
-              final CreditCard _creditCard = CreditCard(
-                number:   card.number,
-                expMonth: card.expMonth,
-                expYear:  card.expYear
-              );
-              StripePayment.createTokenWithCard(_creditCard).then((token) async {
-                final url = 'http://localhost:9000/api/stripe/payment';
-                await http.post(
-                  url,
-                  body: {
-                    'name':          'TEST TOMINAGA',
-                    'email':         'test@ezweb.ne.jp',
-                    'phone':         '09012345678',
-                    'paymentMethod': token
-                  }
-                );
-              });
-            } else {
-              print('処理が通りませんでした。');
-            }
-          }
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CardForm(card: card, formKey: formKey),
+          TextButton(
+              child: Text("決済する"),
+              onPressed: () async {
+                if (formKey.currentState.validate()) {
+                  formKey.currentState.save();
+                  final CreditCard _creditCard = CreditCard(
+                      number:   card.number,
+                      expMonth: card.expMonth,
+                      expYear:  card.expYear
+                  );
+                  StripePayment.createTokenWithCard(_creditCard).then((token) async {
+                    final url = 'http://localhost:9000/api/stripe/payment';
+                    await http.post(
+                        url,
+                        body: {
+                          'name':          'TEST TOMINAGA',
+                          'email':         'test@ezweb.ne.jp',
+                          'phone':         '09012345678',
+                          'paymentMethod': token
+                        }
+                    );
+                  });
+                } else {
+                  print('処理が通りませんでした。');
+                }
+              }
+          )
+        ],
+      )
     );
   }
 }
