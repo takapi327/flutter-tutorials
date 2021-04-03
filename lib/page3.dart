4ae0e1220d251402858694b2d40fd784263dc357
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-04-03 16:01:38 +0900

    Fix Create Stripe Customer

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:stripe_payment/stripe_payment.dart';
@@ -14,27 +16,43 @@ class Page3 extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           CardForm(card: card, formKey: formKey),
-          TextButton(
+          FloatingActionButton(
               child: Text("決済する"),
               onPressed: () async {
                 if (formKey.currentState.validate()) {
                   formKey.currentState.save();
                   final CreditCard _creditCard = CreditCard(
-                      number:   card.number,
-                      expMonth: card.expMonth,
-                      expYear:  card.expYear
+                    number:   card.number,
+                    expMonth: card.expMonth,
+                    expYear:  card.expYear
                   );
-                  StripePayment.createTokenWithCard(_creditCard).then((token) async {
-                    final url = 'http://localhost:9000/api/stripe/payment';
-                    await http.post(
-                        url,
-                        body: {
-                          'name':          'TEST TOMINAGA',
-                          'email':         'test@ezweb.ne.jp',
-                          'phone':         '09012345678',
-                          'paymentMethod': token
-                        }
+                  final PaymentMethodRequest _paymentMethodRequest = PaymentMethodRequest(
+                    card: _creditCard
+                  );
+                  StripePayment.setOptions(
+                    StripeOptions(
+                      publishableKey: "pk_test_51IZrtWJd0VKBQGZhjcHLHLyzjVnt850eXPu9GGgyVKgeC0D6QPY30KkaLWtgXcVstMHrUkXYsElC7UdCDzxeihbj00vnFMsF5c"
+                    )
+                  );
+                  StripePayment.createPaymentMethod(_paymentMethodRequest).then((paymentMethod) async {
+                    final url     = 'http://10.0.2.2:9000/api/stripe/payment';
+                    final headers = {'content-type': 'application/json'};
+                    final body    = json.encode({
+                      "name":          "TEST TAKAHIKO",
+                      "email":         "test-test@ezweb.ne.jp",
+                      "phone":         "09000001234",
+                      "paymentMethod": paymentMethod.id
+                    });
+                    http.Response resp = await http.post(
+                      url,
+                      headers: headers,
+                      body:    body
                     );
+                    if (resp.statusCode != 200) {
+                      print("====================");
+                      print(resp.body);
+                      print("====================");
+                    }
                   });
                 } else {
                   print('処理が通りませんでした。');
